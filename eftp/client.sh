419d90246f442b79dfef0018a3a980051d9284cc
Jan Camon <jan.camon@enti.cat>
2023-11-27 19:47:59 +0100

    Protocolo hasta el paso 21

--- eftp/client.sh
+++ eftp/client.sh
@@ -1,69 +1,89 @@
 #!/bin/bash
-SERVER="localhost"
+SERVER="10.65.0.57"
+IP=`ip address | grep inet | grep enp0s3 | cut -d " " -f 6 | cut -d "/" -f 1`
 PORT=3333
+
+echo "EFTP 1.0" $IP | nc $SERVER $PORT
 TIMEOUT=1
-FILE_NAME="fary1.txt"
-HASH=`cat "$FILE_NAME" | md5sum | cut -d " " -f 1`
+echo "Cliente de EFTP"
+
 echo "(1) Send"
-echo "EFTP 1.0" | nc $SERVER $PORT
+echo "EFTP 1.0 $IP" | nc $SERVER $PORT
 
 echo "(2) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT `
+DATA=`nc -l -p $PORT -w $TIMEOUT`
 echo $DATA
+sleep 1
 
-echo "(5) Test & Send (Handshake)"
+echo "(5) Test & Send"
 if [ "$DATA" != "OK_HEADER" ]
-then
+then 
 	echo "ERROR 1: BAD HEADER"
-	echo "KO_HEADER"
 	exit 1
 fi
-
+echo "OK_HEADER"
 echo "BOOOM"
-sleep 1
+sleep 1 
 echo "BOOOM" | nc $SERVER $PORT
 
 echo "(6) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT `
+DATA=`nc -l -p $PORT -w $TIMEOUT`
 echo $DATA
+sleep 1
 
-echo "(9) test"
-if [ "$DATA" != "OK_HANDSHAKE"  ]
+echo "(9) Test"
+if [ "$DATA" != "OK_HANDSHAKE" ]
 then
-	echo "Error 2: BAD HANDSHAKE"
-	echo "BAD_HANDSHAKE"
+	echo "ERROR 2: BAD HANDSHAKE"
+	sleep 1
+	echo "KO_HANDSHAKE" | nc $SERVER $PORT
 	exit 2
 fi
-
 sleep 1
-echo "(10) Send"
-echo "FILE_NAME fary1.txt $HASH " | nc $SERVER $PORT
+
+echo "(10) Send" 
+FILE_NAME="fary1.txt"
+FILEMD5=`echo "$FILE_NAME" | md5sum | cut -d " " -f 1`
+echo "FILE_NAME $FILE_NAME $FILEMD5" | nc $SERVER $PORT
+sleep 1
 
 echo "(11) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT `
+DATA=`nc -l -p $PORT -w $TIMEOUT`
+echo $DATA
+sleep 1
+echo "(14) Test & Send" 
+if [ "$DATA" != "OK_FILE_NAME" ]
+then 
+	echo "ERROR 3: BAD FILE_NAME"
+	exit 3
+fi 
+sleep 1
+cat imgs/"$FILE_NAME" | nc $SERVER $PORT
+echo "(15) Listen"
+DATA=`nc -l -p $PORT -w $TIMEOUT`
 echo $DATA
 
-echo "(14) Test & Send"
-if [ $DATA != "OK_FILE_NAME" ]
-then
-	echo "Error 3: BAD FILE NAME PREFIX"
-	exit 3
-fi
-sleep 1
-cat imgs/fary1.txt | nc $SERVER $PORT
-
-echo "(15) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT `
+echo "(18) Test & Send"
 
 if [ "$DATA" != "OK_DATA" ]
 then
-	echo "Error 4: BAD DATA"
+	echo "ERROR 4: BAD DATA"
 	exit 4
 fi
 
 echo "(18) Send"
+FILE_MD5=`cat imgs/fary1.txt | md5sum | cut -d " " -f 1`
+echo "FILE_MD5 $FILE_MD5" | nc $SERVER $PORT
 
 echo "(19) Listen"
+DATA=`nc -l -p $PORT -w $TIMEOUT`
+echo "$DATA"
 
-echo "Fin"
+echo "(21) Test"
+if [ "$DATA" != "OK_FILE_MD5" ]
+then
+	echo "ERROR: FILE MD5"
+	exit 5
+fi
+echo "FIN"
 exit 0
--- eftp/server.sh
+++ eftp/server.sh
@@ -1,14 +1,22 @@
-CLIENT=`ip a | grep inet | head -n 3 | tail -n 1 | cut -d " " -f 6 | cut -d "/" -f 1`
-PORT=3333
 TIMEOUT=1
+PORT=3333
+
 echo "Servidor de EFTP"
 
 echo "(0) Listen"
-DATA=`nc -l -p $PORT  -w $TIMEOUT`
-echo $DATA
+
+DATA=`nc -l -p $PORT -w $TIMEOUT`
+
+PREFIX=`echo $DATA | cut -d " " -f 1`
+VERSION=`echo $DATA | cut -d " " -f 2`
+
+echo $DATA 
+
+sleep 1
 
 echo "(3) Test & Send"
-if [ "$DATA" != "EFTP 1.0" ]
+
+if [ "$PREFIX $VERSION" != "EFTP 1.0" ]
 then
 	echo "ERROR 1: BAD HEADER"
 	sleep 1
@@ -16,77 +24,118 @@ then
 	exit 1
 fi
 
+CLIENT=`echo $DATA | cut -d " " -f 3`
+
+if [ "$CLIENT" = "" ]
+then
+	echo "ERROR: NO IP"
+	exit 1
+	echo "KO_HEADER" | nc $CLIENT $PORT
+
 echo "OK_HEADER"
-sleep 1 
+sleep 1
 echo "OK_HEADER" | nc $CLIENT $PORT
 
 echo "(4) Listen"
-DATA=`nc -l -p $PORT  -w $TIMEOUT`
 
+DATA=`nc -l -p $PORT -w $TIMEOUT`
 
-echo "(7) Test & Send (Handshake)"
+echo $DATA
+
+sleep 1
+
+echo "(7) Test & Send"
+
 if [ "$DATA" != "BOOOM" ]
-then
-	echo "ERROR 2: BAD HANDSHAKE"
+then 
+	echo "ERROR 2 : BAD HANDSHAKE"
 	sleep 1
-	echo "BAD_HANDSHAKE" | nc $CLIENT $PORT
+	echo "KO_HANDSHAKE" | nc $CLIENT $PORT
 	exit 2
 fi
-
 sleep 1
 echo "OK_HANDSHAKE" | nc $CLIENT $PORT
 
 echo "(8) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT`
+
+DATA=`nc -l -p $PORT -w $TIMEOUT` 
 echo $DATA
 
+PREFIX=`echo "$DATA" | cut -d " " -f 1`
+echo "$PREFIX"
+
 echo "(12) Test & Store & Send"
-PREFIX=`echo $DATA | cut -d " " -f 1`
-if  [ "$PREFIX" != "FILE_NAME" ]
+if [ "$PREFIX" != "FILE_NAME" ]
 then 
-	echo "ERROR 3: BAD FILE NAME PREFIX"
+	echo "ERROR 3: BAD FILE_NAME"
 	sleep 1
-	echo "KO_FILE_NAME" | nc $CLIENT 3333
+	echo "KO_FILE_NAME" | nc $CLIENT $PORT
 	exit 3
 fi
+sleep 1
+echo "OK_FILE_NAME"| nc $CLIENT $PORT
 
 FILE_NAME=`echo "$DATA" | cut -d " " -f 2`
 FILE_MD5=`echo "$DATA" | cut -d " " -f 3`
-MD5_LOCAL=`echo "$FILE_NAME" | md5sum | cut -d " " -f 1 `
+FILE_MD5_LOCAL=`echo "$FILE_NAME" | md5sum | cut -d " " -f 1`
 
-echo "Testing Hash"
-if [ "$FILE_MD5" != "$MD5_LOCAL" ]
-then
-	echo "Error 3:BAD FILE MD5"
+if [ "$FILE_MD5" != "$FILE_MD5_LOCAL" ]
+then 
+	echo "ERROR 3: BAD FILE NAME MD5"
 	sleep 1
-	echo "KO_FILE_NAME" | nc "$CLIENT" 3333
+	echo "KO_FILE_NAME" | nc $CLIENT $PORT
 	exit 3
 fi
-
-DATA=`cat inbox/$FILE_NAME `
-
 echo "OK_FILE_NAME" | nc $CLIENT $PORT
 
-echo "(13)Listen"
-DATA=`nc -l -p $PORT  -w $TIMEOUT`
+echo "(13) Listen"
 
-echo "(16) Store & Send"
+nc -l -p $PORT -w $TIMEOUT > inbox/$FILE_NAME
+DATA=`cat inbox/fary1.txt`
+echo $DATA
+
+echo "(16) Test & Store & Send"
+
 if [ "$DATA" == "" ]
-then
-	echo "Error 4: EMPTY DATA"
+then 
+	echo "ERROR 4: BAD DATA"
 	sleep 1
 	echo "KO_DATA" | nc $CLIENT $PORT
 	exit 4
 fi
 
-echo $DATA > inbox/$FILE_NAME
-
 sleep 1
-echo "OK_DATA" | nc $CLIENT 3333
+
+echo "OK_DATA" | nc $CLIENT $PORT
 
 echo "(17) Listen"
+
 DATA=`nc -l -p $PORT -w $TIMEOUT`
 echo $DATA
+FILE_MD5_NAME=`echo $DATA | cut -d " " -f 1`
 
-echo "Fin"
+echo "(20) Test & Send"
+
+if [ "FILE_MD5" != "$FILE_MD5_NAME" ]
+then 
+	echo "ERROR 5: BAD FILE_MD5"
+	sleep 1
+	echo "BAD FILE_MD5" | nc $CLIENT $PORT
+	exit 5
+fi
+echo "OK_FILE_MD5" | nc $CLIENT $PORT
+
+FILE_MD5=`echo $DATA | cut -d " " -f 2`
+FILE_HASH=`cat inbox/fary1.txt | md5sum | cut -d " " -f 1`
+
+if [ "$FILE_MD5" != "$FILE_HASH" ]
+then 
+	echo "ERROR 5: BAD FILE_MD5"
+	sleep 1
+	echo "BAD FILE_MD5" | nc $CLIENT $PORT
+	exit 5
+fi
+echo "OK_FILE_MD5" | nc $CLIENT $PORT
+
+echo "FIN"
 exit 0
